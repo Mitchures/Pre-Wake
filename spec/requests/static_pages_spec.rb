bc64ad9c754aaedf31ad652f15998fab1186b56b
Mitchell Hollander <MitchellSH@live.com>
2014-06-13 13:13:32 -0500

    forgot a 'end' in a spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -17,6 +17,7 @@ describe "Static pages" do
     it "should not have a custom page title" do
       visit '/static_pages/home'
       expect(page).not_to have_title('| Home')
+    end
   end
   describe "About page" do
 
