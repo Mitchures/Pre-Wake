723a8cbb10478584e915a41dab814de1cc6ac275
Mitchell Hollander <MitchellSH@live.com>
2014-06-13 18:49:01 -0500

    added user sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+
 root  'static_pages#home'
+match '/signup',  to: 'users#new', via: 'get'
 match '/about', to: 'static_pages#about', via: 'get'
 match '/contact', to: 'static_pages#contact', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
