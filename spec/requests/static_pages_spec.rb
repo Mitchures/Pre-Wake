4b7b5826545c16d9119d1b44a36ace1952d466da
Mitchell Hollander <MitchellSH@live.com>
2014-06-13 15:42:07 -0500

    implemented rails routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,37 +7,37 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have the content 'Pre Wake'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content('Pre Wake')
     end
     it "should have the right title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title("#{base_title}")
     end
     it "should not have a custom page title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).not_to have_title('| Home')
     end
   end
   describe "About page" do
 
     it "should have the content 'About'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About')
     end
     it "should have the right title" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title("#{base_title} | About")
     end
   end
   describe "Contact page" do
 
     it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_content('Contact')
     end
     it "should have the right title" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_title("#{base_title} | Contact")
     end
   end
