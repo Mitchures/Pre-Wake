9f3f9e38b6a4601d7776b7c0aa59cd99dc7ece38
Mitchell Hollander <MitchellSH@live.com>
2014-06-14 17:09:01 -0500

    added password_digest to user spec

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'fog', '~> 1.22.1'
 gem 'pg'
 gem 'bootstrap-sass', '2.3.2.0'
 gem 'sprockets', '2.11.0'
+gem 'bcrypt-ruby', '3.1.2'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.3'
 # Use Uglifier as compressor for JavaScript assets
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140614021519) do
+ActiveRecord::Schema.define(version: 20140614220724) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,9 @@ ActiveRecord::Schema.define(version: 20140614021519) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
