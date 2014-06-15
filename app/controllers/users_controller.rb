9a2e50ae24bff2c38d7380459fd70c9747ea41b4
Mitchell Hollander <MitchellSH@live.com>
2014-06-14 23:49:48 -0500

    finished user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to the Pre Wake!"
       redirect_to @user
     else
       render 'new'
