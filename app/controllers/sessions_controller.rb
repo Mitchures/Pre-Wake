49e5848098ee548181da40d58c7426d49dabc532
Mitchell Hollander <MitchellSH@live.com>
2014-06-16 16:33:59 -0500

    commented out twitter impl.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,14 +4,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    auth = request.env["omniauth.auth"]
-    user = User.find_by_uid(auth["uid"]) || User.create_with_omniauth(auth)
-    session[:user_id] = user.uid
-    redirect_to root_url, :notice => "Signed in!"
+    # auth = request.env["omniauth.auth"]
+    # user = User.find_by_uid(auth["uid"]) || User.create_with_omniauth(auth)
+    # session[:user_id] = user.uid
+    # redirect_to root_url, :notice => "Signed in!"
   end
 
   def destroy
-    session[:user_id] = nil
-    redirect_to root_url, :notice => "Signed out!"
+    # session[:user_id] = nil
+    # redirect_to root_url, :notice => "Signed out!"
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,16 +5,16 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   has_secure_password
 
-  def self.create_with_omniauth(auth)
-    create! do |user|
-      user.provider = auth["provider"]
-      user.uid = auth["uid"]
-      user.name = auth["info"]["name"]
-      user.nickname = auth["info"]["nickname"]
-      user.token = auth["credentials"]["token"]
-      user.secret = auth["credentials"]["secret"]
-    end
-  end
+  # def self.create_with_omniauth(auth)
+  #   create! do |user|
+  #     user.provider = auth["provider"]
+  #     user.uid = auth["uid"]
+  #     user.name = auth["info"]["name"]
+  #     user.nickname = auth["info"]["nickname"]
+  #     user.token = auth["credentials"]["token"]
+  #     user.secret = auth["credentials"]["secret"]
+  #   end
+  # end
 
 
 end
