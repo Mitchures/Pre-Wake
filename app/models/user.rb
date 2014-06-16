64765b736cbbfb1cba084a53f895b87d324be9ac
Mitchell Hollander <MitchellSH@live.com>
2014-06-16 10:43:38 -0500

    testing log in on heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, length: { minimum: 6 }
+
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.provider = auth["provider"]
+      user.uid = auth["uid"]
+      user.name = auth["info"]["name"]
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 resources :users
+resources :sessions, only: [:new, :create, :destroy]
 root  'static_pages#home'
 match '/signup',  to: 'users#new', via: 'get'
+match '/signin',  to: 'sessions#new',         via: 'get'
+match "/auth/:provider/callback" => "sessions#create", via: 'get'
+match '/signout', to: 'sessions#destroy',     via: 'delete'
 match '/about', to: 'static_pages#about', via: 'get'
 match '/contact', to: 'static_pages#contact', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
